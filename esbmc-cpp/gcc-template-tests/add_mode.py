2993f1788898043d869c0bede468095844f466b2
kunjsong01 <kunjian90@gmail.com>
2023-08-05 13:08:30 +0100

    prepare cor core_lang_benchmarks

--- esbmc-cpp/gcc-template-tests/add_mode.py
+++ esbmc-cpp/gcc-template-tests/add_mode.py
@@ -4,7 +4,7 @@ for path in Path('./').rglob('test.desc'):
     print(path)
     f = open(path,'r')
     lines = f.readlines()[:-1]
-    lines.append("<item_10_mode>KNOWNBUG</item_10_mode>" + "\n")
+    lines.append("<item_10_mode>CORE</item_10_mode>" + "\n")
     lines.append("</test-case>")
     f.close()
     f = open(path,'w')
